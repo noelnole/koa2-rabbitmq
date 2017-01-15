242d0ee05e3d8ad8bc0067f16cf573340f5c729b
noel <trushn45@gmail.com>
2017-01-15 22:34:18 +0100

    feature receive

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const koa = require('koa');
+var router = require('koa-router')();
+
+const send = require('./lib/send');
+const receive = require('./lib/receive');
+const api = require('./lib/api');
+
+const app = new koa()
+
+router.get('/receiving', async (ctx,next) => {
+  this.body = await receive();
+})
+
+app
+  .use(router.routes())
+  .use(api.routes())
+  .use(router.allowedMethods());
+
+app.listen(5000);
+console.log("Listen in port 5000");
+
+
+//RabbitMQhttp://localhost:15672/#/connections
+//pwd guest
+//user guest
